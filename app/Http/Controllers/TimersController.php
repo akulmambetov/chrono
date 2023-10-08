52d322d8206f5f1fc89b196a595ec5881f260c52
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-08 19:50:34 +0600

    Create model, controller, migration for Timer

---
+++ app/Http/Controllers/TimersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Timer;
+use Illuminate\Http\Request;
+
+class TimersController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Timer $timer)
+    {
+    }
+
+    public function edit(Timer $timer)
+    {
+    }
+
+    public function update(Request $request, Timer $timer)
+    {
+    }
+
+    public function destroy(Timer $timer)
+    {
+    }
+}
