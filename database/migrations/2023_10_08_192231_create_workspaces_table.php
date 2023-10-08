9153c72e546f30906a0d8b7fff9bcd16e428c757
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-08 19:28:14 +0600

    Create model, controller, migration for Workspace

---
+++ database/migrations/2023_10_08_192231_create_workspaces_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create(
+            'workspaces',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->foreignId('user_id')->constrained();
+                $table->softDeletes();
+                $table->timestamps();
+            }
+        );
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('workspaces');
+    }
+};
