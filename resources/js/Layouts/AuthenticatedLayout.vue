13bcdc3efccd214130d3c8f386d46705d65512a3
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-23 01:10:45 +0600

    Siderbar close button

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,7 +7,15 @@ import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import {Link} from '@inertiajs/vue3';
 import Sidebar from "@/Layouts/Partials/Sidebar.vue";
+
 const showingNavigationDropdown = ref(false);
+
+const showSideBar = ref(true);
+
+const closeSideBar = () => {
+  showSideBar.value = false;
+};
+
 </script>
 
 <template>
@@ -15,11 +23,18 @@ const showingNavigationDropdown = ref(false);
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
       <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
         <!-- Primary Navigation Menu -->
-        <Sidebar></Sidebar>
+        <Sidebar :showSideBar="showSideBar" @close="closeSideBar"></Sidebar>
 
         <div class="px-4 sm:px-6 lg:px-8 border-b">
-          <div class="flex justify-start h-12 pl-60">
+          <div class="flex justify-start h-12" :class="{'pl-60': showSideBar}">
             <div class="flex">
+              <div class="m-1 p-2 group rounded cursor-pointer hover:bg-gray-300 flex items-center"
+                   v-if="!showSideBar"
+                   @click="showSideBar = !showSideBar">
+                <box-icon class="fill-gray-500 group-hover:fill-gray-950" name='dock-left'></box-icon>
+
+              </div>
+
               <!-- Navigation Links -->
               <div class="hidden space-x-8 sm:-my-px sm:flex">
                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
@@ -126,7 +141,7 @@ const showingNavigationDropdown = ref(false);
       </header>
 
       <!-- Page Content -->
-      <main class="pl-60">
+      <main :class="{'pl-60': showSideBar}">
         <slot/>
       </main>
     </div>
--- resources/js/Layouts/Partials/Sidebar.vue
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -1,11 +1,55 @@
 <script setup>
-import UserBar from "@/Layouts/Partials/UserBar.vue";
 import Workspaces from "@/Layouts/Partials/Workspaces.vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import {Link, usePage} from "@inertiajs/vue3";
+import {computed, ref} from 'vue'
+
+const page = usePage()
+
+const user = computed(() => page.props.auth.user)
+
+defineProps({
+  showSideBar: {
+    type: Boolean,
+    default: true
+  }
+})
+
+const emit = defineEmits(['close']);
+
+const emitSideBar = () => {
+  emit('close');
+}
 </script>
 
 <template>
-  <div class="max-w-7xl w-64 bg-gray-50 border-r h-full fixed left-0 flex flex-col">
-    <UserBar></UserBar>
-    <Workspaces></Workspaces>
-  </div>
+  <Transition
+    enter-from-class="opacity-0 translate-x-4"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100 translate-x-4"
+    leave-to-class="opacity-0"
+  >
+    <div v-show="showSideBar" class="max-w-7xl w-64 bg-gray-50 border-r h-full fixed left-0 flex flex-col">
+      <div class="px-4 h-12 flex justify-between items-center">
+        <div class="rounded cursor-pointer p-1 hover:bg-gray-300 content-center">
+          <Link :href="route('home')">
+            <ApplicationLogo class="h-6"></ApplicationLogo>
+          </Link>
+        </div>
+        <div class="flex gap-3">
+          <div class="group rounded cursor-pointer p-1 hover:bg-gray-300 flex items-center"
+               @click="emitSideBar">
+            <box-icon class="fill-gray-500 group-hover:fill-gray-950" name='dock-left'></box-icon>
+
+          </div>
+
+          <Link :href="route('profile.edit')"
+                class="flex items-center group rounded cursor-pointer p-1 hover:bg-gray-300">
+            <box-icon class="fill-gray-500 group-hover:fill-gray-950" name='user'></box-icon>
+          </Link>
+        </div>
+      </div>
+      <Workspaces></Workspaces>
+    </div>
+  </Transition>
 </template>
