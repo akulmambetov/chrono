bbca9f7c6a0563ba607531b327fb0618bd878a0a
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-22 19:31:51 +0600

    Use MustVerifyEmail trait for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Lab404\Impersonate\Models\Impersonate;
@@ -16,6 +18,7 @@ class User extends Authenticatable
     use Notifiable;
     use HasRoles;
     use Impersonate;
+    use MustVerifyEmail;
 
     /**
      * The attributes that are mass assignable.
@@ -47,4 +50,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function workspaces(): HasMany
+    {
+        return $this->hasMany(Workspace::class, 'user_id');
+    }
 }
