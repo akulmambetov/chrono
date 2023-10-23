42bfa723a22dbac7730ca578b424850d833368eb
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-23 19:26:58 +0600

    Default workspace

--- resources/js/Layouts/Partials/Workspaces.vue
+++ resources/js/Layouts/Partials/Workspaces.vue
@@ -4,13 +4,13 @@
 
   const page = usePage();
 
-  const workspaces = computed(() => page.props.auth.workspaces);
+  const defaultWorkspace = page.props.auth.default_workspace;
 </script>
 
 <template>
   <div class="flex flex-col justify-center gap-1">
-    <div v-for="workspace in workspaces" class="bg-white cursor-pointer hover:bg-gray-50 rounded py-1 mx-6 text-gray-900 border text-center text-sm">
-      <Link :href="route('workspaces.show', workspace)">{{ workspace.name }}</Link>
+    <div v-if="defaultWorkspace" class="bg-white cursor-pointer hover:bg-gray-50 rounded py-1 mx-6 text-gray-900 border text-center text-sm">
+      <Link :href="route('workspaces.show', defaultWorkspace)">{{ defaultWorkspace.name }}</Link>
     </div>
     <div class="bg-white hover:bg-gray-50 cursor-pointer rounded py-1 mx-6 text-gray-900 border text-center text-sm">
       <Link :href="route('workspaces.create')">Add new</Link>
