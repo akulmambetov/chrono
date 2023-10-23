0844fe7bf9d21e2a0bb24eb7e40ff5aa7ce98791
Atai Kulmambetov <atai.k@intelliants.com>
2023-10-23 18:33:02 +0600

    Null safe operator if workspaces doesn't exist

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => $request->user(),
-                'workspaces' => $request->user()->workspaces
+                'workspaces' => $request->user()->workspaces ?? null
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy())->toArray(), [
