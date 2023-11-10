5655b72930b66ddac9be4cc9fe14e246db86bbbd
Atai Kulmambetov <atai.k@intelliants.com>
2023-11-10 17:32:33 +0600

    style: add active classes

--- resources/js/Layouts/Partials/Workspaces.vue
+++ resources/js/Layouts/Partials/Workspaces.vue
@@ -12,24 +12,30 @@
   <div>
     <div class="flex flex-col justify-center gap-3 mt-6">
       <Link v-if="defaultWorkspace" class="flex group flex-row hover:text-blue-500 items-center cursor-pointer hover:bg-gray-300 rounded-md p-2 mx-6 text-gray-900 text-sm"
+            :class="{'text-blue-500 bg-gray-300 font-bold' : route().current('workspaces.*')}"
             :href="route('workspaces.show', defaultWorkspace)">
-        <InboxStackIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"/>
+        <InboxStackIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"
+                        :class="{'text-blue-500' : route().current('workspaces.*')}"/>
         {{ defaultWorkspace.name }}
       </Link>
 
       <Link class="flex group flex-row hover:text-blue-500 items-center cursor-pointer hover:bg-gray-300 rounded-md p-2 mx-6 text-gray-900 text-sm"
+            :class="{'text-blue-500 bg-gray-300 font-bold' : route().current('timer.*')}"
             :href="route('timer.index')">
-        <ClockIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"/>
+        <ClockIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"
+                   :class="{'text-blue-500' : route().current('timer.*')}"/>
         Timer
       </Link>
 
       <Link class="flex group flex-row hover:text-blue-500 items-center cursor-pointer hover:bg-gray-300 rounded-md p-2 mx-6 text-gray-900 text-sm">
-        <UserIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"/>
+        <UserIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"
+                  :class="{'text-blue-500' : route().current('users.*')}"/>
         Users
       </Link>
 
       <Link class="flex group flex-row hover:text-blue-500 items-center cursor-pointer hover:bg-gray-300 rounded-md p-2 mx-6 text-gray-900 text-sm">
-        <ClipboardDocumentListIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"/>
+        <ClipboardDocumentListIcon class="h-5 mr-3 text-gray-500 group-hover:text-blue-500"
+                                   :class="{'text-blue-500' : route().current('reports.*')}"/>
         Reports
       </Link>
     </div>
