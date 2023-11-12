fe4b3cddc9bbc9964341751ebe3eb06cd40b81df
Atai Kulmambetov <atai.k@intelliants.com>
2023-11-12 16:49:45 +0600

    style: fix for table layout

--- resources/js/Components/TableLayout.vue
+++ resources/js/Components/TableLayout.vue
@@ -11,9 +11,9 @@ defineProps({
 </script>
 
 <template>
-  <div class="relative overflow-x-auto">
+  <div class="relative overflow-x-auto rounded-md">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
         <slot name="head"></slot>
       </thead>
       <tbody>
