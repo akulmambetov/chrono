aba60093cd014f857d0f7a9f1bdca49d13ebf8d6
Atai Kulmambetov <atai.k@intelliants.com>
2023-11-12 16:34:10 +0600

    feat: create TimerRequest feat: crud for Timers

--- app/Http/Controllers/TimersController.php
+++ app/Http/Controllers/TimersController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TimerRequest;
 use App\Models\Timer;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class TimersController extends Controller
@@ -17,27 +17,38 @@ class TimersController extends Controller
         return Inertia::render('Timer/Index', compact('timers'));
     }
 
-    public function create(Request $request)
+    public function create()
     {
+        return Inertia::render('Timer/Create',[
+            'model' => new Timer()
+        ]);
     }
 
-    public function store(Request $request)
+    public function store(TimerRequest $request)
     {
+        Timer::create($request->validated());
+
+        return to_route('timer.index');
     }
 
     public function show(Timer $timer)
     {
+        return Inertia::render('Timer/Show', compact('timer'));
     }
 
     public function edit(Timer $timer)
     {
     }
 
-    public function update(Request $request, Timer $timer)
+    public function update(TimerRequest $request, Timer $timer)
     {
+        $timer->update($request->validated());
     }
 
     public function destroy(Timer $timer)
     {
+        $timer->delete();
+
+        return to_route('timer.index');
     }
 }
---
+++ app/Http/Requests/TimerRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TimerRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string',
+            'user_id' => 'required',
+            'workspace_id' => 'required',
+            'started_at' => 'required|date'
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
