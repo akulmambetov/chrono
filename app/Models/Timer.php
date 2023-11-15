eef5b9c5b2700b9b87d0a325994ca196a78fbb7b
Atai Kulmambetov <atai.k@intelliants.com>
2023-11-15 23:39:52 +0600

    feat: add difference time attribute to timer

--- app/Models/Timer.php
+++ app/Models/Timer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -14,6 +15,16 @@ class Timer extends Model
       'started_at' => 'datetime',
       'stopped_at' => 'datetime'
     ];
+
+    protected $appends = ['difference_time'];
+
+    protected function differenceTime(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $this->started_at->diff($this->stopped_at)->format('%H:%I:%S'),
+            set: fn($value) => $value,
+        );
+    }
 
     public function scopeAssociated(Builder $query):void
     {
