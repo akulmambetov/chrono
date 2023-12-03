ab351c1ef8a6ebbd6e80453c61f9c5bea66fd0d5
Atai Kulmambetov <atai.k@intelliants.com>
2023-12-03 23:33:30 +0600

    refactor: hide table if workspaces doesnt exist

--- resources/js/Pages/Workspace/Index.vue
+++ resources/js/Pages/Workspace/Index.vue
@@ -42,30 +42,32 @@ const closeModal = () => {
         </PrimaryButton>
       </Link>
 
-      <TableLayout class="mt-6">
-        <template #head>
-          <tr>
-            <th class="px-6 py-3">Name</th>
-            <th class="px-6 py-3">Default</th>
-            <th class="px-6 py-3">Action</th>
-          </tr>
-        </template>
+      <div v-if="workspaces.length">
+        <TableLayout class="mt-6">
+          <template #head>
+            <tr>
+              <th class="px-6 py-3">Name</th>
+              <th class="px-6 py-3">Default</th>
+              <th class="px-6 py-3">Action</th>
+            </tr>
+          </template>
 
-        <template #body>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="workspace in workspaces">
-            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              {{ workspace.name }}
-            </th>
-            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          <template #body>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="workspace in workspaces">
+              <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ workspace.name }}
+              </th>
+              <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               <span class="p-1 rounded text-white"
-              :class="{'bg-green-500': workspace.default, 'bg-red-500': !workspace.default}">{{ workspace.default ? 'Yes' : 'No' }}</span>
-            </th>
-            <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Edit
-            </th>
-          </tr>
-        </template>
-      </TableLayout>
+                    :class="{'bg-green-500': workspace.default, 'bg-red-500': !workspace.default}">{{ workspace.default ? 'Yes' : 'No' }}</span>
+              </th>
+              <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Edit
+              </th>
+            </tr>
+          </template>
+        </TableLayout>
+      </div>
     </div>
   </AuthenticatedLayout>
 </template>
