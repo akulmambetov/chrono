00fe96f0b51c978ddcb2db10088ef57f5d83a6a0
Atai Kulmambetov <a.kulmambetov@yandex.ru>
2023-12-12 00:39:17 +0600

    feat: create new model Role for constants

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class Role extends SpatieRole
+{
+    use HasFactory;
+
+    public const ADMIN = 'admin';
+    public const OWNER = 'owner';
+    public const PARTICIPANT = 'participant';
+}
