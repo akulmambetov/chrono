742257e6fa188f122c5d371e468b9b1dee893aa0
Atai Kulmambetov <atai.k@intelliants.com>
2024-01-04 17:52:16 +0600

    refactor: create factory for Workspace model

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,6 +11,8 @@ use voku\helper\ASCII;
 
 class Workspace extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'user_id', 'default'];
 
     public function scopeMine(Builder $query): Builder
